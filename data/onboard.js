63190f35a1230ae14e80dc99d4cf15ec1bc67a81
gaiyadev <gaiyaobed94@gmail.com>
2021-05-04 12:55:37 +0100

    add Onboard screen

--- data/onboard.js
+++ data/onboard.js
@@ -1,17 +1,23 @@
+import { images } from "../constants";
+const { onboarding1, onboarding2, onboarding3 } = images;
+
 export const onBoardings = [
   {
+    id: 1,
     title: "Let's Travelling",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut",
     img: onboarding1,
   },
   {
+    id: 2,
     title: "Navigation",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut",
     img: onboarding2,
   },
   {
+    id: 3,
     title: "Destination",
     description:
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut",
--- navigation/routes.js
+++ navigation/routes.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import OnboardScreen from "../screens/onboard/OnboardScreen";
+import HomeScreen from "../screens/onboard/HomeScreen";
 
 const OnboardStack = createStackNavigator();
 
@@ -14,6 +15,7 @@ const Onboard = () => {
           component={OnboardScreen}
           options={{ headerShown: false }}
         />
+        <OnboardStack.Screen name="HomeScreen" component={HomeScreen} />
       </OnboardStack.Navigator>
     </NavigationContainer>
   );
